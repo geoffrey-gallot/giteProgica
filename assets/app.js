fbde4455cf967f0c80d3bcdf59ac788e1a5c2bd7
geoffrey-gallot <geoffrey.gallot@gmail.com>
2021-06-24 09:38:29 +0200

    ajout scrollreveal

--- assets/app.js
+++ assets/app.js
@@ -11,3 +11,19 @@ import './styles/app.css';
 // start the Stimulus application
 import './bootstrap';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+//import ScrollReveal
+import ScrollReveal from 'scrollreveal';
+
+
+let cards = document.querySelectorAll(".card");
+
+
+ScrollReveal().reveal(cards, {
+    delay: 250,
+    duration: 250,
+    easing: "ease-in-out",
+    interval: 400,
+    reset: true,
+
+});
